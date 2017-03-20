421e26ec9f71799ee08a0c4d0941f17249412c66
caozh90 <caozh90@foxmail.com>
2017-03-20 09:29:07 +0800

    20170320

---
+++ templates/procedure/常用SQL.sql
@@ -0,0 +1,5 @@
+select DISTINCT(father)
+from "EBGCFE"."UI_MANUAL_BOM" where father not in (
+select father from "EBGCFE"."UI_MANUAL_BOM" where father LIKE_REGEXPR '^[A-Za-z0-9]+$'
+) order by father
+
